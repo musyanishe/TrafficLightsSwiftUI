ec254618431d73413d7f340f65e17780364daab0
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-05-13 09:00:43 +0300

    edit contentView using ternar operator

--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -13,44 +13,32 @@ enum CurrentLight {
 
 struct ContentView: View {
     @State private var buttonTitle = "Start"
-    
-    @State private var redCircleOpacity = 0.3
-    @State private var yellowCircleOpacity = 0.3
-    @State private var greenCircleOpacity = 0.3
-    
     @State private var currentLight = CurrentLight.red
     
     private func buttonPressed() {
-        let lightIsOn = 1.0
-        let lightIsOff = 0.3
         
         switch currentLight {
         case .red:
-            redCircleOpacity = lightIsOn
-            greenCircleOpacity = lightIsOff
             currentLight = .yellow
         case .yellow:
-            redCircleOpacity = lightIsOff
-            yellowCircleOpacity = lightIsOn
             currentLight = .green
         case .green:
-            yellowCircleOpacity = lightIsOff
-            greenCircleOpacity = lightIsOn
             currentLight = .red
         }
     }
 }
 
 extension ContentView {
+    
     var body: some View {
         ZStack{
             Color.black
                 .ignoresSafeArea()
 
             VStack(spacing: 10) {
-                ColorCircleView(color: .red, opacity: redCircleOpacity)
-                ColorCircleView(color: .yellow, opacity: yellowCircleOpacity)
-                ColorCircleView(color: .green, opacity: greenCircleOpacity)
+                ColorCircleView(color: .red, opacity: currentLight == .red ? 1 : 0.3 )
+                ColorCircleView(color: .yellow, opacity: currentLight == .yellow ? 1 : 0.3)
+                ColorCircleView(color: .green, opacity: currentLight == .green ? 1 : 0.3)
                 Spacer()
                 
                 ButtonView(buttonTitle: buttonTitle) {
